23f753f54fb654da2102f50cdcb0613bbb7fa589
Vinicius Tabosa <94081542+ViniciusTabosa@users.noreply.github.com>
2025-12-13 21:20:07 -0300

    Update pedido_doacao.py
    Adicionando novas verificações

--- pedido_doacao.py
+++ pedido_doacao.py
@@ -2,12 +2,25 @@
 from datetime import date
 
 class PedidoDoacao:
-    def __init__(self, titulo, descricao):
+    def __init__(self, usuario, titulo, descricao):
         data = date.today()
-        self.__titulo = titulo
-        self.__descricao = descricao
+        self.__recebedor = usuario
+        if (not isinstance(titulo, str)) or (not titulo.strip()):
+            self.__titulo = titulo
+        else:
+            raise ValueError('Título inválido')
+
+        if (not isinstance(descricao, str)) or (not descricao.strip()):
+            self.__descricao = descricao
+        else:
+            raise ValueError('Descrição inválido')
+
         self.__data = f'{data.day}/{data.month}/{data.year}'
         self.__pedidoAberto = True
+
+    @property
+    def recebedor(self):
+        return self.__recebedor
 
     @property
     def titulo(self):
@@ -16,7 +29,7 @@ class PedidoDoacao:
     @titulo.setter
     def titulo(self, novo_titulo):
         if (not isinstance(novo_titulo, str)) or (not novo_titulo.strip()):
-            raise ValueError('Texto inválido')
+            raise ValueError('Título inválido')
         self.__titulo = novo_titulo
 
     @property
@@ -26,18 +39,12 @@ class PedidoDoacao:
     @descricao.setter
     def descricao(self, nova_descricao):
         if (not isinstance(nova_descricao, str)) or (not nova_descricao.strip()):
-            raise ValueError('Texto inválido')
+            raise ValueError('Descrição inválida')
         self.__descricao = nova_descricao
 
     @property
     def data(self):
         return self.__data
-
-    # @data.setter
-    # def data(self, nova_data):
-    #     if (not isinstance(nova_data, str)) or (not nova_data.strip()):
-    #         raise ValueError('Data inválido')
-    #     self.__data = nova_data
 
     @property
     def pedidoAberto(self):
